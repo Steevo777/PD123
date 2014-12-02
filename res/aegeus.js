61e9000f123f9b6e6ef8d091b366d4cba16410f7
Steevo777 <steveh@a-information.com>
2014-12-02 11:07:59 -0500

    First Version

---
+++ res/aegeus.js
@@ -0,0 +1,88 @@
+function Pwd() {
+	that = this;
+}
+
+//<script>
+//    var encrypted = CryptoJS.TripleDES.encrypt("Message", "Secret Passphrase");
+//    var decrypted = CryptoJS.TripleDES.decrypt(encrypted, "Secret Passphrase");
+//</script>
+
+var scrtPasPhrase = "4NemosNautilus";
+
+Pwd.prototype.setup = function(callback) {
+
+	//First, setup the database
+	this.db = window.openDatabase("Pwd", 1, "Pwd", 1000000);
+	this.db.transaction(this.initDB, this.dbErrorHandler, callback);
+
+}
+
+
+
+//Genric database error handler. Won't do anything for now.
+Pwd.prototype.dbErrorHandler = function(e) {
+	console.log('DB Error');
+	console.dir(e);
+}
+
+//I initialize the database structure
+Pwd.prototype.initDB = function(t) {
+	t.executeSql('create table if not exists pwd(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT, image TEXT, published DATE)');
+}
+
+Pwd.prototype.getEntries = function(start,callback) {
+	console.log('Running getEntries');
+	if(arguments.length === 1) callback = arguments[0];
+
+	this.db.transaction(
+		function(t) {
+			t.executeSql('select id, title, body, image, published from pwd order by published desc',[],
+				function(t,results) {
+					callback(that.fixResults(results));
+				},this.dbErrorHandler);
+		}, this.dbErrorHandler);
+
+}
+
+Pwd.prototype.getEntry = function(id, callback) {
+
+	this.db.transaction(
+		function(t) {
+			t.executeSql('select id, title, body, image, published from pwd where id = ?', [id],
+				function(t, results) {
+					callback(that.fixResult(results));
+				}, this.dbErrorHandler);
+			}, this.dbErrorHandler);
+
+}
+
+//No support for edits yet
+Pwd.prototype.saveEntry = function(data, callback) {
+
+var encpTitle = CryptoJS.TripleDES.encrypt(data.title, scrtPasPhrase);
+console.dir(data);
+	this.db.transaction(
+		function(t) {
+			t.executeSql('insert into pwd(title,body,image,published) values(?,?,?,?)', [encpTitle, data.body, data.image, new Date()],
+			function() { 
+				callback();
+			}, this.dbErrorHandler);
+		}, this.dbErrorHandler);
+}
+
+//Utility to convert record sets into array of obs
+Pwd.prototype.fixResults = function(res) {
+	var result = [];
+	for(var i=0, len=res.rows.length; i<len; i++) {
+		var row = res.rows.item(i);
+		result.push(row);
+	}
+	return result;
+}
+
+//I'm a lot like fixResults, but I'm only used in the context of expecting one row, so I return an ob, not an array
+Pwd.prototype.fixResult = function(res) {
+	if(res.rows.length) {
+		return res.rows.item(0);
+	} else return {};
+}
---
+++ res/aifuncs.js
@@ -0,0 +1,53 @@
+//Include script - <script type="text/javascript" src="res/aifuncs.js"></script>
+
+function dateFmt(dateStr) {
+    if(!dateStr) return "";
+	dateStr = new Date(dateStr);
+    var res = dayName(dateStr) + " " + monthName(dateStr) + " " + dateStr.getDate() + "," + dateStr.getFullYear() + " ";
+    var hour = dateStr.getHours()+1;
+    var ampm = "AM";
+	if(hour === 12) ampm = "PM";
+    if(hour > 12){
+        hour-=12;
+        ampm = "PM";
+    }
+    var minute = dateStr.getMinutes()+1;
+    if(minute < 10) minute = "0" + minute;
+    res += hour + ":" + minute + " " + ampm;
+    return res;
+}
+
+function dayName(dateStr) {
+	if(!dateStr) return "";
+	var weekday = new Array(7);
+	weekday[0]=  "Sun";
+	weekday[1] = "Mon";
+	weekday[2] = "Tue";
+	weekday[3] = "Wed";
+	weekday[4] = "Thu";
+	weekday[5] = "Fri";
+	weekday[6] = "Sat";
+	
+	var dayName = weekday[dateStr.getDay()];
+	return dayName;
+}
+
+function dayName(dateStr) {
+	if(!dateStr) return "";
+    var monthNames = new Array(12); 
+    weekday[0]=  "Jan";
+	weekday[1] = "Feb";
+	weekday[2] = "Mar";
+	weekday[3] = "Apr";
+	weekday[4] = "May";
+	weekday[5] = "Jun";
+	weekday[6] = "Jul";
+	weekday[7] = "Aug";
+	weekday[8] = "Sep";
+	weekday[9] = "Oct";
+	weekday[10] = "Nov";
+	weekday[11] = "Dec";
+	
+    var monthName =monthNames[dateStr.getMonth()]);
+	return monthName;
+}
