6a1c5e385957afcbda127bb8ac63832b6c067d83
Steevo777 <steveh@a-information.com>
2014-12-02 11:53:40 -0500

    Remove Cam

--- res/index.js
+++ res/index.js
@@ -99,22 +99,6 @@ $(document).on("pageload", "#entryPage", function(e) {
 
 $(document).on("pageload", "#addPage", function(e) {
 
-	function onCamSuccess(imgdata) {
-		console.log(imgdata);
-		$("#entryPicture").val(imgdata);
-		$("#imgPreview").attr("src", imgdata);
-	}
-	
-	function onCamFail(e) {
-		alert("error picture");
-		console.log('camFail');console.dir(e);
-		navigator.notification.alert("Sorry, something went wrong.", null, "Oops!");
-	}
-	
-	$("#takePicture").on("touchstart", function(e) {
-		e.preventDefault();
-		navigator.camera.getPicture(onCamSuccess, onCamFail, {quality:50, destinationType:Camera.DestinationType.FILE_URI});
-	});
 	
 	$("#addEntrySubmit").on("touchstart", function(e) {
 		e.preventDefault();
